fd4f79d4e43e442886b83f89904cd549bdb178e2
Aleena-Jomon <68261629+Aleena-Jomon@users.noreply.github.com>
2022-03-01 13:45:57 +0530

    refactor ui, add font, align buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:vibration/vibration.dart';
-import 'package:torch/torch.dart';
+import 'package:torch_light/torch_light.dart';
 import 'dart:io';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,6 +43,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Color.fromARGB(255, 0, 209, 17),
+
+        // Define the default font family.
+        fontFamily: 'popins',
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 28.0, fontStyle: FontStyle.normal),
+          bodyText2: TextStyle(fontSize: 20.0, fontFamily: 'Hind'),
+        ),
+      ),
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
@@ -78,9 +95,9 @@ class _MyHomePageState extends State<MyHomePage> {
   void lit() {
     for (int i = 0; i < code.length - 1; i++) {
       int time = timimg[code[i]];
-      time > 0 ? Torch.turnOn() : 1;
+      time > 0 ? TorchLight.enableTorch() : 1;
       sleep(Duration(milliseconds: time));
-      Torch.turnOff();
+      TorchLight.disableTorch();
       sleep(Duration(milliseconds: 100));
     }
   }
@@ -91,14 +108,31 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('MorseLight'),
       ),
       body: Column(children: <Widget>[
+        SizedBox(
+          //Use of SizedBox
+          height: 100,
+        ),
         TextField(
           controller: texty,
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Enter Your message here',
+          ),
+        ),
+        SizedBox(
+          //Use of SizedBox
+          height: 100,
         ),
         RaisedButton(
             onPressed: encoder,
             child: Text(
               "Convert To Morse Code",
             )),
+        SizedBox(
+          //Use of SizedBox
+          height: 10,
+        ),
         Text(
           code,
         ),
@@ -108,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
             RaisedButton(
                 onPressed: vibe,
                 child: Text(
-                  "🥁",
+                  "📳",
                 )),
             RaisedButton(
                 onPressed: lit,
