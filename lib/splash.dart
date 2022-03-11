433cee19cb502ed823fbe108b82e730c59123431
Abhijithabhi <abhiabhijith367@gmail.com>
2022-03-11 20:48:08 +0530

    *****

---
+++ lib/splash.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class splash extends StatefulWidget {
+  const splash({Key? key}) : super(key: key);
+
+  @override
+  State<splash> createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SecondScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text(
+            'Splash Screen',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
